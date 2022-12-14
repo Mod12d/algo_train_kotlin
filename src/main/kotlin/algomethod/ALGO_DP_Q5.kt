4441072c8e3cb5913e3eac73a244e12d364caec5
terumi <terumi8.neu@icloud.com>
2022-12-14 23:42:47 +0900

    test

--- src/main/kotlin/algomethod/ALGO_DP_Q5.kt
+++ src/main/kotlin/algomethod/ALGO_DP_Q5.kt
@@ -1,23 +1,15 @@
 //https://algo-method.com/tasks/306
 import kotlin.math.*
-fun perm(n: Int, k: Int): Int {
-    require(n > 0 && k >= 0)
-    return (n - k + 1 .. n).fold(1) { acc, i -> acc * i }
-}
-fun comb(n: Int, k: Int): Int {
-    require(n > 0 && k >= 0)
-    val fact = (2..k).fold(1) { acc, i -> acc * i }
-    return perm(n, k) / fact
-}
+
 fun main() {
     val (n,m) = readLine()!!.split(" ").map{it.toInt()}
     val a =  readLine()!!.split(" ").map{it.toInt()}.toMutableList()
     val dp : MutableList<Int> = ArrayList()
     dp.add(a[0])
-    for(i in 1.. n-1){
-        for(j in 1..m-1){
+    for(i in 1..n){
+        for(j in 1..m+1){
             if(i-j>=0)dp[i] = min(dp[i],dp[i-j]+ a[i]*j)
         }
     }
-    //print(dp[])
+    print(dp[n-1])
 }
