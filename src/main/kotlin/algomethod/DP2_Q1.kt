d026d5229afd70ac4b813780f396151de33e7870
terumi <terumi8.neu@icloud.com>
2022-12-24 20:59:39 +0900

    #6 init

--- src/main/kotlin/algomethod/DP2_Q1.kt
+++ src/main/kotlin/algomethod/DP2_Q1.kt
@@ -2,21 +2,20 @@
 
 fun main() {
     val a =  readLine()!!.split(" ").map{it.toInt()}.toMutableList()
-    val dp : MutableList<MutableList<Int>> = ArrayList()
+    var dp : MutableList<MutableList<Int>> = MutableList(4){ index -> mutableListOf(0,0,0,0) }
     // replace later
-        dp[0]= mutableListOf(0,0,0,0)
     // 0 case
     for(i in 0..3){
         dp[0][i] = a[i]
     }
     // n case
     for(i in 1..3){
-        for(j in 0..3 ){
+        for(j in 0..2 ){
             if(j==0) dp[i][j] = dp[i-1][j]+dp[i-1][j]
             if(j==1 or 2) dp[i][j] = dp[i-1][j]+dp[i-1][j]+dp[i-1][j+1]
             if(j==3) dp[i][j] = dp[i-1][j]+dp[i-1][j+1]
         }
     }
 
-    print(dp[3][3])
+    print(dp)
 }
